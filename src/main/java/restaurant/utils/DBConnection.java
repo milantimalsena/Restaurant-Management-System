96ee2e3aa605a15b0f3e0b78dda6582c42eb626a
milantimalsena <milantimalsena87@gmail.com>
2026-04-10 22:04:27 +0545

    Db con ncection

---
+++ src/main/java/restaurant/utils/DBConnection.java
@@ -0,0 +1,18 @@
+package restaurant.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+        private static final String DB_URL = "jdbc:mysql://localhost:3306/restro ";
+        private static final String DB_USER = "root";
+        private static String DB_PASSWORD = "1234";
+
+        public static Connection getConnection() throws SQLException {
+            return DriverManager.getConnection(DB_URL, DB_USER,DB_PASSWORD);
+        }
+
+    }
+}
